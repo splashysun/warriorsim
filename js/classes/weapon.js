36b5a960267713e2a4b9041fcd4e52df910d5ba5
GuybrushGit <turinpt@gmail.com>
2024-11-21 21:17:11 +0000

    cleave fix

--- js/classes/weapon.js
+++ js/classes/weapon.js
@@ -124,7 +124,7 @@ class Weapon {
         let mod = 1;
         dmg = rng(this.mindmg + this.bonusdmg, this.maxdmg + this.bonusdmg) + (this.player.stats.ap / 14) * this.speed + this.player.stats.moddmgdone;
         if (heroicstrike) dmg += heroicstrike.bonus;
-        if (heroicstrike && this.player.heroicbonus) mod = 1.25;
+        if (heroicstrike && heroicstrike instanceof HeroicStrike && this.player.heroicbonus) mod = 1.25;
         return dmg * this.modifier * this.player.stats.dmgmod * mod + this.player.stats.moddmgtaken;
     }
     avgdmg() {
